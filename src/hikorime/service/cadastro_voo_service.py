84d543d3ab3f89eeca2103c0565e1a7e7c9752c3
sudo-invers <dhonatanjose8@gmail.com>
2026-01-29 15:20:36 -0300

    adicionado um service de exemplo 	new file:   src/hikorime/service/cadastro_voo_service.py

---
+++ src/hikorime/service/cadastro_voo_service.py
@@ -0,0 +1,35 @@
+from hikorime.service.base_service import BaseService
+from hikorime.repository.repository_querys import RepositoryQuerys
+
+
+class CadastroVooService(BaseService):
+    """docstring for CadastroVooService."""
+
+    def __init__(self, repo: RepositoryQuerys):
+        self.repo = repo
+
+    def save(
+        self,
+        data_saida,
+        data_cheg,
+        hora_saida,
+        hora_chega,
+        local_saida,
+        destino,
+        id_piloto,
+        aviao,
+        quant_vagas,
+        status_voo: StatusVoo,
+    ):
+        return self.repo.save(
+            data_saida=data_saida,
+            data_cheg=data_cheg,
+            hora_saida=hora_saida,
+            hora_chega=hora_chega,
+            local_saida=local_saida,
+            destino=destino,
+            id_piloto=id_piloto,
+            aviao=aviao,
+            quant_vagas=quant_vagas,
+            status_voo=status_voo,
+        )
