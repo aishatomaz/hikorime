fe215c9a9d6f3e136d611d22568daea5d852449e
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-02-12 18:53:28 -0300

    Refazer propeties para models/voo
    ValueError trocado para TypeError nos casos adequados

--- src/hikorime/models/voo.py
+++ src/hikorime/models/voo.py
@@ -16,7 +16,7 @@ class Voo:
     @data_hora_partida.setter
     def data_hora_partida(self, data_hora_partida_valida):
         if not isinstance(data_hora_partida_valida, datetime):
-            raise ValueError("A data de partida deve ser do tipo date.")
+            raise TypeError("A data de partida deve ser do tipo date.")
         self.__data_hora_partida = data_hora_partida_valida
 
     @property
@@ -26,7 +26,7 @@ class Voo:
     @data_hora_chegada.setter
     def data_hora_chegada(self, data_hora_chegada_valida):
         if not isinstance(data_hora_chegada_valida, datetime):
-            raise ValueError("A data e hora de chegada deve ser do tipo datetime.")
+            raise TypeError("A data e hora de chegada deve ser do tipo datetime.")
         self.__data_hora_chegada = data_hora_chegada_valida
 
     @property
@@ -37,7 +37,7 @@ class Voo:
     def data_hora_chegada_prevista(self, data_hora_chegada_prevista_valida):
 
         if not isinstance(data_hora_chegada_prevista_valida, datetime):
-            raise ValueError("A data e hora de chegada previsto deve ser do tipo datetime.")
+            raise TypeError("A data e hora de chegada previsto deve ser do tipo datetime.")
         self.__data_hora_chegada_prevista = data_hora_chegada_prevista_valida
 
     @property
@@ -47,7 +47,10 @@ class Voo:
     @local_origem.setter
     def local_origem(self, local_origem_valida):
         if not isinstance(local_origem_valida, str):
-            raise ValueError("O local de origem deve ser do tipo str.")
+            raise TypeError("O local de origem deve ser do tipo str.")
+        
+        if not local_origem_valida:
+                raise ValueError("O local de origem não pode ser vazio.")
 
         self.__local_origem = local_origem_valida    
         
@@ -58,10 +61,10 @@ class Voo:
     @assentos_ocupados.setter
     def assentos_ocupados(self, assentos_ocupados_valida):
         if not isinstance(assentos_ocupados_valida, int):
-            raise ValueError("A quantidade de passageiros deve ser do tipo int.")
+            raise TypeError("A quantidade de assentos ocupados deve ser do tipo int.")
         
         if assentos_ocupados_valida <= 0:
-            raise ValueError("A quantidade de passageiros deve ser maior que zero.")
+            raise ValueError("A quantidade de assentos ocupados deve ser maior que zero.")
         self.__assentos_ocupados = assentos_ocupados_valida
 
     @property
@@ -71,5 +74,8 @@ class Voo:
     @local_destino.setter
     def local_destino(self, local_destino_valida):
         if not isinstance(local_destino_valida, str):
-            raise ValueError("O local de destino deve ser do tipo str.")
+            raise TypeError("O local de destino deve ser do tipo str.")
+        
+        if not local_destino_valida:
+            raise ValueError("O local de destino não pode ser vazio.")
         self.__local_destino = local_destino_valida
