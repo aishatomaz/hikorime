71305b50b66191f39c7318da787b84043e09067e
sudo-invers <dhonatanjose8@gmail.com>
2026-02-16 19:04:44 -0300

    Refeito a classe RepositoryCOnnection, e melhorado sua legibilidade;
    Refeito a maioria das docstrings de repositiryQuerys;
    
     On branch dhonatan
     Changes to be committed:
    	modified:   src/hikorime/repository/repository_connection.py
    	modified:   src/hikorime/repository/repository_querys.py
    	modified:   src/hikorime/service/compra_service.py

--- src/hikorime/repository/repository_connection.py
+++ src/hikorime/repository/repository_connection.py
@@ -28,16 +28,16 @@ class RepositoryConnection:
         conn.row_factory = sqlite3.Row  # Vou poder usar dicionarios e tuplas com isso
         return conn
 
-    def save(self, query: str, params: Tuple = ()) -> int | None:
+    def save(self, query: str, params: Tuple | dict = ()) -> int | None:
         """
         Funcao de criar novas entradas na tabela.
 
         SqlType:
-            POST
+            INSERT
 
         Args:
             query: Query sql pura.
-            params: Parametros da query em tupla.
+            params: Parametros da query em tupla ou dict.
 
         Returns:
             Retorna o id do que foi criado (se tiver erro, retorna None.)
@@ -49,7 +49,7 @@ class RepositoryConnection:
             conn.commit()
             return cursor.lastrowid
 
-    def get_one(self, query: str, params: Tuple = ()) -> Optional[Dict]:
+    def get_one(self, query: str, params: Tuple | dict = ()) -> Optional[Dict]:
         """
         Funcao de retirar apenas uma entradas da tabela.
 
@@ -58,28 +58,28 @@ class RepositoryConnection:
 
         Args:
             query: Query sql pura.
-            params: Parametros da query em tupla.
+            params: Parametros da query em tupla ou dict.
 
         Returns:
             registro afetado
         """
 
-        with self._connect() as conn:  # Usa oque esta em _connect
+        with self._connect() as conn:
             cursor = conn.cursor()
             cursor.execute(query, params)
             row = cursor.fetchone()
             return dict(row) if row else None
 
-    def get_many(self, query: str, params: Tuple = ()) -> List[Dict]:
+    def get_many(self, query: str, params: Tuple | dict = ()) -> List[Dict]:
         """
-        Funcao de retirar varias entradas da tabela.
+        Funcao de retirar varias entradas da tabela (Inclusive get_all).
 
         SqlType:
             GET
 
         Args:
             query: Query sql pura.
-            params: Parametros da query em tupla.
+            params: Parametros da query em tupla ou dict.
 
         Returns:
             Registros afetados
@@ -91,16 +91,16 @@ class RepositoryConnection:
             rows = cursor.fetchall()
             return [dict(row) for row in rows]
 
-    def update(self, query: str, params: Tuple = ()) -> int:
+    def update(self, query: str, params: Tuple | dict = ()) -> int:
         """
         Funcao que modifica parametros de linhas da tabela.
 
         SqlType:
-            UPDATE
+            UPDATE(PUT), LIKE
 
         Args:
             query: Query sql pura.
-            params: Parametros da query em tupla.
+            params: Parametros da query em tupla ou dict.
 
         Returns:
             quantidade de linhas afetadas.
@@ -111,7 +111,7 @@ class RepositoryConnection:
             conn.commit()
             return cursor.rowcount
 
-    def delete(self, query: str, params: Tuple = ()) -> int:
+    def delete(self, query: str, params: Tuple | dict = ()) -> int:
         """
         Funcao que deleta uma linha da tabela.
 
@@ -120,7 +120,7 @@ class RepositoryConnection:
 
         Args:
             query: Query sql pura.
-            params: Parametros da query em tupla.
+            params: Parametros da query em tupla ou dict.
         Returns:
             Quantidade de Linhas afetadas
         """
