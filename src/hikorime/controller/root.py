7cb75e4990ee20def47fa5ca8c5f96eccdd54543
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-02-27 14:27:18 -0300

    configurar ui/static

--- src/hikorime/controller/root.py
+++ src/hikorime/controller/root.py
@@ -3,6 +3,6 @@ from hikorime.ui.engine import HikorimeUI
 
 base_router = APIRouter()
 
-base_router.get("/")
+@base_router.get("/")
 def index(request: Request):
     return HikorimeUI.render("index.html", request)
