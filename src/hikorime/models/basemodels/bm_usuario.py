73376e6cfd622c0080abf3aadf085dd9f4189dca
Aisha <aisha.tomaz@aluno.ufca.edu.br>
2026-03-01 23:20:24 -0300

    Validação de CPF e herança de bm_usuario

---
+++ src/hikorime/models/basemodels/bm_usuario.py
@@ -0,0 +1,34 @@
+from pydantic import EmailStr, Field, validator
+from hikorime.models.enums.tipo_usuario import TipoUsuario
+from hikorime.models.basemodels.bm_login import PasswordBase
+
+class UsuarioBase(PasswordBase):
+ 
+    """
+    Modelo de entrada de dados de usuário - parte de cadastro de novo usuário.
+    Herda a validação de senha de PasswordBase.
+    """
+    
+    nome: str
+    email: EmailStr
+    cpf: str = Field(..., min_length=11, max_length=11)
+    tipo: TipoUsuario
+
+    @validator('cpf')
+    def validate_cpf(cls, cpf):
+        cpf = ''.join(filter(str.isdigit, cpf))
+
+        if len(cpf) != 11:
+            raise ValueError('CPF deve ter 11 dígitos.')
+
+        if cpf == cpf[0] * 11:
+            raise ValueError('CPF inválido.')
+
+        for i in range(9, 11):
+            soma = sum(int(cpf[j]) * ((i + 1) - j) for j in range(i))
+            digito = 11 - (soma % 11)
+            if digito > 9:
+                digito = 0
+            if int(cpf[i]) != digito:
+                raise ValueError('CPF inválido.')
+        return cpf
