0980c4a98bda1d6b18ea61ffe1cacc70b4509c6f
sudo-invers <dhonatanjose8@gmail.com>
2026-03-07 16:38:33 -0300

    Adicionado aeronave_service funcional

--- src/hikorime/service/aeronave_service.py
+++ src/hikorime/service/aeronave_service.py
@@ -1,17 +1,22 @@
-from base_service import BaseService
-from repository.repository_querys import RepositoryQuerys
-from models.basemodels.bm_aeronave import Aeronave
+from hikorime.repository.repository_querys import RepositoryQuerys
+from hikorime.models.basemodels.bm_aeronave import Aeronave
+from fastapi.exceptions import HTTPException
+from hikorime.service.base_service import BaseService
 
 
 class AeronaveService(BaseService):
     def __init__(self):
-        super().__init__(RepositoryQuerys("aeronaves"))
+        self.repo = RepositoryQuerys("aeronaves")
 
-    def cadastrar_aeronave(self, dados: Aeronave):
+    def cadastrar_aeronave(self, aeronave_data: dict):
         """
         Cadastra uma nova aeronave usando os campos do BaseModel
         """
-        return self.save(dados)
+        try:
+            aeronave = Aeronave(**aeronave_data)
+            return self.save(aeronave)
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=f"Erro ao criar voo: {str(e)}")
 
     def listar_aeronaves(self):
         """
